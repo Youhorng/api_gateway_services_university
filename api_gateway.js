acbdb3fe85663a0c256b4c56bad2e06cd55a338f
Youhorng <2023221kean@aupp.edu.kh>
2025-05-30 16:14:04 +0700

    Add public ip ec2

--- api_gateway.js
+++ api_gateway.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer();
 //REDIRECT TO THE STUDENT MICROSERVICE
 app.use('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:2000' });
+    proxy.web(req, res, { target: 'http://3.85.162.27:2000' });
 })
 
 //REDIRECT TO THE TEACHER MICROSERVICE
 app.use('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:3000' });
+    proxy.web(req, res, { target: 'http://13.220.204.53:3000' });
 })
 
 app.listen(4000, () => {
